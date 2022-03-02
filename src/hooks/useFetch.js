1e5a2c8e98ee2fc3016f2a65fddb2e4a1867b53d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-02 21:20:52 +0100

    created useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState} from 'react'
+
+export const useFetch = url => {
+  const [data, setData] = useState([])
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    const fetchData = async () => {
+      setIsPending(true)
+
+      try{
+        const res = await fetch(url, { signal: controller.signal })
+        if(!res.ok) {
+          throw new Error(res.statusText)
+        }
+        const json = await res.json()
+
+        setIsPending(false)
+        setData(json)
+        setError(null)
+        
+      } catch(err) {
+        if(err.name === "AbortError") {
+          console.log("the fetch was aborted")
+        } else {
+          setIsPending(false)
+          setError('Could not fetch user profiles')
+          console.log(err.message)
+        }
+        
+      }
+
+    }
+
+    fetchData()
+
+    return () => {
+      controller.abort()
+    }
+  }, [url])
+
+  return { data, isPending, error }
+
+}
