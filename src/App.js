ee71235349c53ab20269eeb80bdb7c531868e628
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-02 11:49:59 +0100

    urls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 
 function App() {
+  // https://api.themoviedb.org/3/movie/550?api_key=821df521d9494e5d28d041685eeaee64
+
+  // images
+  // https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
   return <div className="App">movie app</div>;
 }
 
