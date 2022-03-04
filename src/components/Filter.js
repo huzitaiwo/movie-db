ebc4bf9d26c627bcd9ffdaec58ee19d17e180857
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-04 21:53:48 +0100

    created Filter.js file

---
+++ src/components/Filter.js
@@ -0,0 +1,12 @@
+// styles
+import './Filter.css'
+
+export default function Filter() {
+  return (
+    <div>
+      <button>All</button>
+      <button>Comedy</button>
+      <button>Action</button>
+    </div>
+  )
+}
