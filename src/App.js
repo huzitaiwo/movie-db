6dbc817dc68db48d53ad17aaf5fd8373697ac99d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-04 21:26:15 +0100

    fetching movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { useFetch } from "./hooks/useFetch"
+
+// styles
+import "./App.css"
 
 function App() {
   // https://api.themoviedb.org/3/movie/550?api_key=821df521d9494e5d28d041685eeaee64
 
   // images
   // https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+  const url = 'https://api.themoviedb.org/3/movie/550?api_key=821df521d9494e5d28d041685eeaee64';
+  const { data: movies, isPending, error } = useFetch(url)
   return <div className="App">movie app</div>;
 }
 
