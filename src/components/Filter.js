78d28aa0ab051bd47a2c19d57278dac0bab1d6f5
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-04 21:59:16 +0100

    styled Filter.js file

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,7 @@ import './Filter.css'
 
 export default function Filter() {
   return (
-    <div>
+    <div className='filter'>
       <button>All</button>
       <button>Comedy</button>
       <button>Action</button>
