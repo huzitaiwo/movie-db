37b7d4764e44fd47703935ac1887669b94d59aee
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-04 22:12:56 +0100

    useEffect

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,25 @@
+import { useEffect } from 'react'
+
 // styles
 import './Filter.css'
 
-export default function Filter() {
+export default function Filter({ movies, setFiltered, genre, setGenre}) {
+
+  useEffect(() => {
+      
+    if(genre === 0) {
+    setFiltered(movies)
+      return;
+    }
+
+  }, [genre])
+
   return (
     <div className='filter'>
-      <button>All</button>
-      <button>Comedy</button>
-      <button>Action</button>
-      <button>Animation</button>
+      <button onClick={() => setGenre(0)}>All</button>
+      <button onClick={() => setGenre(35)}>Comedy</button>
+      <button onClick={() => setGenre(28)}>Action</button>
+      <button onClick={() => setGenre(16)}>Animation</button>
     </div>
   )
 }
